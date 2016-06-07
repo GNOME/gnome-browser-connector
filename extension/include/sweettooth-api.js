8233b332efdfe28e41f54b938ed98c4a5aafa4db
Yuri Konotopov <ykonotopov@gmail.com>
2016-06-07 20:18:42 +0300

    Prepare release: no changes are planned for connector. Hardcode version

--- extension/include/sweettooth-api.js
+++ extension/include/sweettooth-api.js
@@ -247,7 +247,8 @@ require(['messages', 'gs-chrome'], function(messages){
 		SweetTooth.shellVersion			= response.shellVersion;
 		SweetTooth.versionValidationEnabled	= response.versionValidationEnabled;
 
-		if(!response.connectorVersion || response.connectorVersion != GS_CHROME_VERSION)
+		if(!response.connectorVersion ||
+			(response.connectorVersion != 6 && response.connectorVersion != 6.1))
 		{
 			if(!response.connectorVersion)
 				response.connectorVersion = GSC.getMessage('older_connector');
