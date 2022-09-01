ef7fd1c1bcae88888c6e7828f6283f9de8fd5f29
Yuri Konotopov <ykonotopov@gnome.org>
2022-09-01 18:46:33 +0400

    Remove extra logic from module init
    Otherwise it will break installation without expected dependencies.
    
    Fixes: https://gitlab.gnome.org/nE0sIghT/gnome-browser-connector/-/issues/5

---
+++ gnome_browser_connector/main.py
@@ -0,0 +1,36 @@
+# SPDX-License-Identifer: GPL-3.0-or-later
+
+import logging
+import os
+import sys
+
+from .application import Application
+from .constants import CONNECTOR_ARG
+from .logs import NameAbbrFilter
+
+def main():
+    logging.basicConfig(
+        format="%(asctime)s: [%(process)d] %(levelname)s %(name_abbr)s %(message)s",
+        level=getattr(
+            logging,
+            os.getenv("GNOME_BROWSER_CONNECTOR_LOGLEVEL", "warning").upper()
+        ),
+        stream=sys.stderr
+    )
+    logging.getLogger().handlers[0].addFilter(NameAbbrFilter())
+    logging.debug('Main')
+
+    app = Application()
+    code = app.run(sys.argv)
+
+    logging.debug('Quit')
+
+    return code
+
+def connector():
+    ensure_argument_exists(CONNECTOR_ARG)
+    return main()
+
+def ensure_argument_exists(argument: str):
+    if f"--{argument}" not in sys.argv:
+        sys.argv.insert(1, f"--{argument}")
